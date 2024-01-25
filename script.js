5eebd7ed9506454a520d8f083ea08d433cc8aab0
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-01-25 21:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
   // your code here
+	let arr = str.split("0").filter((st)=>st.length !== 0)
+
+	let obj = {
+		firstName : arr[0],
+		lastName : arr[1],
+		id : arr[2]
+	}
+	return obj
 };
 
 // Do not change the code below
